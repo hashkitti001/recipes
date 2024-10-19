c717ddf48717a5fc148f55de67a1a16f7b4d3d95
hashkitti001 <iJeddy5.0@gmail.com>
2024-10-19 16:22:20 +0100

    ...

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,5 +1,6 @@
 import {BrowserRouter , Routes , Route} from 'react-router-dom'
 import Landing from './pages/landing/Landing'
+import AuthPage from './pages/landing/auth/AuthPage'
 
 
 const App = () => {
@@ -7,6 +8,7 @@ const App = () => {
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Landing/>}/>
+        <Route path="/auth" element={<AuthPage/>}/>
       </Routes>
     </BrowserRouter>
   )
